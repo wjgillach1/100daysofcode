8442804e6933a796337aa802c63c65b17230f7dd
William Gillach <>
2022-10-04 03:46:45 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+print ("""Will Gillach
+October 3rd 2022
+I am signing up for Replit's 100 days of Python challenge!
+I will make sure to spend some time every day coding along, for a minimum of 10 minutes a day.
+I'll be using Replit, an amazing online IDE so I can do this from my phone wherever I happend to be. No excuses for not coding from the middle of a field!
+I am feeling pretty good!
+You can follow my progress at replit.com/@williamgillach""")
